5b4952ea3b8135768800cf1d1ea11a45b185fe6d
hk1 <kushal.23@lpu.in>
2025-11-24 15:47:07 +0530

    "Commited"

--- ABustoUdayland.java
+++ ABustoUdayland.java
@@ -1,40 +1,35 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
-public class ABustoUdayland{
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+public class ABustoUdayland {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
 
         List<String> busLayout = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            busLayout.add(br.readLine());
+            busLayout.add(sc.nextLine());
         }
 
-        boolean foundSpot = false;
+        boolean found = false;
 
         for (int i = 0; i < n; i++) {
             String row = busLayout.get(i);
             if (row.charAt(0) == 'O' && row.charAt(1) == 'O') {
                 busLayout.set(i, "++|" + row.substring(3));
-                foundSpot = true;
+                found = true;
                 break;
             }
             if (row.charAt(3) == 'O' && row.charAt(4) == 'O') {
                 busLayout.set(i, row.substring(0, 3) + "++");
-                foundSpot = true;
+                found = true;
                 break;
             }
         }
 
-        if (foundSpot) {
+        if (found) {
             System.out.println("YES");
-            for (String row : busLayout) {
-                System.out.println(row);
-            }
+            for (String s : busLayout) System.out.println(s);
         } else {
             System.out.println("NO");
         }
--- AMinimalCoprime.java
+++ AMinimalCoprime.java
@@ -1,25 +1,19 @@
 import java.util.*;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+
 public class AMinimalCoprime {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
         StringBuilder sb = new StringBuilder();
+
         while (t-- > 0) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            long l = Long.parseLong(st.nextToken());
-            long r = Long.parseLong(st.nextToken());
+            long l = sc.nextLong();
+            long r = sc.nextLong();
 
             long ans;
             if (l == 1) {
-                if (r == 1) {
-                    ans = 1;
-                } else {
-                    ans = r - 1;
-                }
+                if (r == 1) ans = 1;
+                else ans = r - 1;
             } else {
                 ans = r - l;
             }
