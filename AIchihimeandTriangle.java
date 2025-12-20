d892dbb2e2cdb7beb4296edd93925f788e80179d
hk1 <kushal.23@lpu.in>
2025-12-21 00:00:09 +0530

    "Commited"

---
+++ AIchihimeandTriangle.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class AIchihimeandTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            long a = sc.nextLong();
+            long b = sc.nextLong();
+            long c = sc.nextLong();
+            long d = sc.nextLong();
+            long x = b;
+            long y = c;
+            long z = c;
+            System.out.println(x + " " + y + " " + z);
+        }
+    }
+}
---
+++ AVustheCossackandaContest.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class AVustheCossackandaContest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int k = sc.nextInt();
+        if (m >= n && k >= n) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
